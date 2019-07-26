b97b5a30ce166dd61132068ae33f1bd15964a2f1
masuhang <masuhangvip>
2019-07-26 18:02:03 +0800

    域名错误的问题 18:01

--- front/h5/config/prod.env.js
+++ front/h5/config/prod.env.js
@@ -2,7 +2,7 @@
 module.exports = {
   NODE_ENV: '"production"',
   BASE_API: '"https://api.pinlala.com/bxb_app"',
-  FRONT_HOST: '"http://activity.pinlala.com/app"',
+  FRONT_HOST: '"http://bxb.pinlala.com/app"',
   RELOAD_ON_CLICK_MENU: true,
   WECHAT_APP_ID: '"wx378dc130adbcf3e3"'
 }
