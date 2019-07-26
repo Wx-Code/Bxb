253bb4b52c8275f059088f0331f233e38e89c0fa
masuhang <masuhangvip>
2019-07-26 17:25:46 +0800

    解决域名和api地址的冲突

--- front/h5/config/test.env.js
+++ front/h5/config/test.env.js
@@ -2,11 +2,7 @@
 module.exports = {
   NODE_ENV: '"test"',
   BASE_API: '"http://testapi.pinlala.com/bxb_app"',
-<<<<<<< HEAD
-  FRONT_HOST: '"http://testbxb.pinlala.com/app"',
-=======
   FRONT_HOST: '"http://testactivity.pinlala.com/app"',
->>>>>>> af0e7a6932d8872333b750aeebbf22cdce371fab
   RELOAD_ON_CLICK_MENU: true,
   WECHAT_APP_ID: '"wxacde7318d98fad96"',
 }
