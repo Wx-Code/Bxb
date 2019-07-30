22140b7f91c4564c60dd13753ccb9762390dc9e0
谭光洪 <499166435@qq.com>
2019-07-30 13:57:47 +0800

    [fix] 修复发送短信每次都发送相同内容的问题

--- api/Shunmai.Bxb.Utilities/Sms/ISmsProvider.cs
+++ api/Shunmai.Bxb.Utilities/Sms/ISmsProvider.cs
@@ -2,10 +2,6 @@
 {
     public interface ISmsProvider
     {
-        string Api { get; set; }
-
-        string Content { get; set; }
-
         ResponseEntity Send(string phoneNo, int count = 4);
 
         ResponseEntity SendCode(string phoneNo, string code);
