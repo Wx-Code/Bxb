0d163ecffe2f0963b947bf9f21dd30cb93d805ee
谭光洪 <499166435@qq.com>
2019-07-30 16:15:26 +0800

    [feature] 完成接口 [POST] /order 的编码工作

---
+++ api/Shunmai.Bxb.Services/Models/SubmitData.cs
@@ -0,0 +1,29 @@
+﻿using Shunmai.Bxb.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shunmai.Bxb.Services.Models
+{
+    public class SubmitData
+    {
+        public TradeHall Hall { get; set; }
+        public User Buyer { get; set; }
+        public User Seller { get; set; }
+        public int RequiredCount { get; set; }
+        public string TradeCode { get; set; }
+        /// <summary>
+        /// 收取手续费的钱包地址
+        /// </summary>
+        public string ServiceFeeReceiveWalletAddr { get; set; }
+        /// <summary>
+        /// 平台钱包地址
+        /// </summary>
+        public string PlatformWalletAddr { get; set; }
+        /// <summary>
+        /// 服务费比例
+        /// </summary>
+        public decimal ServiceFeeRate { get; set; }
+        public decimal ServiceFee => ServiceFeeRate * RequiredCount;
+    }
+}
