90d55ab1151192d0d5d3b200b9281e2fd41ed150
谭光洪 <499166435@qq.com>
2019-07-30 12:12:11 +0800

    [fix] 修复接口 /common/trade/rules 报错的问题

---
+++ api/Shunmai.Bxb.Test.Common/Models/SystemConfigExt.cs
@@ -0,0 +1,14 @@
+﻿using Shunmai.Bxb.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Shunmai.Bxb.Test.Common.Models
+{
+    public class SystemConfigExt : SystemConfig
+    {
+        [Key]
+        public override string ConfigName { get; set; }
+    }
+}
