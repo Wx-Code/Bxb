5118e4f26451b99f190756ba6c4cbfe773940513
wuxing <wuxing@yunfangtan.com>
2019-08-02 13:37:27 +0800

    Update Defaults.cs
    设置剩余时间

--- api/Shunmai.Bxb.Api.App/Constansts/Defaults.cs
+++ api/Shunmai.Bxb.Api.App/Constansts/Defaults.cs
@@ -8,5 +8,15 @@ namespace Shunmai.Bxb.Api.App.Constants
         /// TOKEN 有效期
         /// </summary>
         public static readonly TimeSpan TOKEN_EXPIRES = TimeSpan.FromMinutes(3 * 24 * 60);
+
+        /// <summary>
+        /// 待转币 剩余 过期时间
+        /// </summary>
+        public static readonly TimeSpan SellerOperating_EXPIRES = TimeSpan.FromHours(2);
+
+        /// <summary>
+        /// 待收款 剩余 过期时间
+        /// </summary>
+        public static readonly TimeSpan BuyerPaying_EXPIRES = TimeSpan.FromHours(24);
     }
 }
